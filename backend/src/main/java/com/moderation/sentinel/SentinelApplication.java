aec8dcf4ee9b09adec5592ec5b9a0822103314be
xaman27x <136126298+xaman27x@users.noreply.github.com>
2025-05-22 03:10:11 +0530

    add: boilerplate code

--- backend/src/main/java/com/moderation/sentinel/SentinelApplication.java
+++ backend/src/main/java/com/moderation/sentinel/SentinelApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 // Entry point of Spring Boot App
 public class SentinelApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SentinelApplication.class, args);
 	}
